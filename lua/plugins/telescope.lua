cb29887a583e5c40518ac1bca4a31f7342147b0c
Hugo Baba <hugobarette@gmail.com>
2025-05-26 21:09:01 +0200

    Add some sweet telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,43 +1,102 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "BurntSushi/ripgrep",
-      "sharkdp/fd",
-    },
-    config = function()
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>f", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
-    end,
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      local actions = require("telescope.actions")
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "BurntSushi/ripgrep",
+            "sharkdp/fd",
+            "nvim-telescope/telescope-ui-select.nvim",
+        },
+        config = function()
+            local builtin = require("telescope.builtin")
+            local actions = require("telescope.actions")
+            local themes = require("telescope.themes")
 
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-        defaults = {
-          mappings = {
-            i = {
-              ["<C-s>"] = actions.select_vertical,
-              ["<C-h>"] = actions.select_horizontal,
-            },
-            n = {
-              ["s"] = actions.select_vertical,
-              ["h"] = actions.select_horizontal,
-            },
-          },
-        },
-      })
-      require("telescope").load_extension("ui-select")
-    end,
-  },
+            require("telescope").setup({
+                extensions = {
+                    ["ui-select"] = {
+                        themes.get_dropdown({}),
+                    },
+                },
+                defaults = {
+                    mappings = {
+                        i = {
+                            ["<C-s>"] = actions.select_vertical,
+                            ["<C-h>"] = actions.select_horizontal,
+                        },
+                        n = {
+                            ["s"] = actions.select_vertical,
+                            ["h"] = actions.select_horizontal,
+                        },
+                    },
+                },
+            })
+
+            -- search for files
+            vim.keymap.set("n", "<leader>ff", function()
+                builtin.find_files {
+                    file_ignore_patterns = {"%.uid", "%.import", "%.tmp"}
+                }
+            end
+            )
+            -- search for scripts
+            vim.keymap.set("n", "<leader>fs", function()
+                builtin.find_files {
+                    file_ignore_patterns = {"%.uid", "%.tscn", "%.import", "%.tres", "%.tmp"},
+                }
+            end
+            )
+
+            -- Search for nvim config
+            vim.keymap.set("n", "<leader>fc", function()
+                builtin.find_files {
+                    cwd = vim.fn.stdpath("config")
+                }
+            end
+            )
+
+            -- Search in the nvim lua plugins source code
+            vim.keymap.set("n", "<leader>fp", function()
+                builtin.find_files {
+                    cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
+                }
+            end
+            )
+
+            -- Live grep in files
+            vim.keymap.set("n", "<leader>gg", function()
+                builtin.live_grep {
+                    file_ignore_patterns = {"%.uid", "%.import", "%.tmp"}
+                }
+            end
+            )
+
+            -- Live grep in scripts
+            vim.keymap.set("n", "<leader>gs", function()
+                builtin.live_grep {
+                    file_ignore_patterns = {"%.uid", "%.tscn", "%.import", "%.tres", "%.tmp"},
+                }
+            end
+            )
+
+            -- Live grep in config files
+            vim.keymap.set("n", "<leader>gc", function()
+                builtin.live_grep {
+                    cwd = vim.fn.stdpath("config")
+                }
+            end
+            )
+
+            -- Live grep nvim lua plugins source code
+            vim.keymap.set("n", "<leader>gp", function()
+                builtin.live_grep {
+                    cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
+                }
+            end
+            )
+
+            require("telescope").load_extension("ui-select")
+        end,
+    },
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,19 +1,36 @@
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set list")
+vim.cmd("set listchars=tab:>·,trail:×")
 vim.cmd("set relativenumber")
 vim.cmd("set number")
 vim.cmd("set splitright")
 vim.cmd("set termguicolors")
 vim.cmd("set fileformats=unix,dos")
-vim.g.fileformats ="unix,dos"
+
+vim.g.fileformats = "unix,dos"
+
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.api.nvim_set_keymap("t", "<ESC>", "<C-\\><C-n>", { noremap = true })
+
+local map = vim.keymap.set
+
 -- Remap demi page up/down to recenter the cursor in the middle of the screen
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true })
+map("n", "<C-d>", "<C-d>zz", { noremap = true })
+map("n", "<C-u>", "<C-u>zz", { noremap = true })
+
+-- Disable arrows in normal mode
+map("n", "<Up>", ":echoe 'No arrows allowed, you fool!'<CR>")
+map("n", "<Down>", ":echoe 'No arrows allowed, you fool!'<CR>")
+map("n", "<left>", ":echoe 'No arrows allowed, you fool!'<CR>")
+map("n", "<Right>", ":echoe 'No arrows allowed, you fool!'<CR>")
+
+-- Macro to create a setter and an associated signal in GDscript
+vim.fn.setreg("z", "yiwA:\rset(value):\r  if value != pa:\r	pa = value\r.pa_changed.emit()\r\risignal pa_changed2k^x2j$")
+
